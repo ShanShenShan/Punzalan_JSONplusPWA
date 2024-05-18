599441d930eea4ccc16f828d12cad5e44dacf21b
ShanShenShan <148319845+ShanShenShan@users.noreply.github.com>
2024-05-18 23:05:36 +0800

    Add files via upload

---
+++ serviceWorker.js
@@ -0,0 +1,26 @@
+const staticDevCoffee = "shan-json"
+const assets = [
+  "/",
+  "/index.html",
+  "/gallery.html",
+  "/about.html",
+  "/style.css",
+  "/pfp.jpg",
+  "/icon.png",
+]
+
+self.addEventListener("install", installEvent => {
+  installEvent.waitUntil(
+    caches.open(staticDevCoffee).then(cache => {
+      cache.addAll(assets)
+    })
+  )
+})
+
+self.addEventListener("fetch", fetchEvent => {
+    fetchEvent.respondWith(
+      caches.match(fetchEvent.request).then(res => {
+        return res || fetch(fetchEvent.request)
+      })
+    )
+  })
